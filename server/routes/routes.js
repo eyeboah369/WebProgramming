01535b9eea0ccb14211673699f643f76b484c889
Evans Yeboah Jr <eyeboahcw@gmail.com>
2020-09-22 23:13:29 -0400

    updates routes

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,15 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user")
 const registerController = require('../controllers/register');
 const loginController = require('../controllers/login');
 
-router.get('/userlist:fname', async (req, res) => {
-    const users = await User.findOne({fname: req.fname});
-    console.log(users);
-    res.send(users);
-})
-
+router.get('/userlist', loginController.getAllUsers);
 router.post('/login', loginController.login);
 router.post('/register', registerController.register);
 
