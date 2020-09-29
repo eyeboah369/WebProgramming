5209e9aec6aa1e50d545a9375781a517b0f617ef
Evans Yeboah Jr <eyeboahcw@gmail.com>
2020-09-29 17:49:06 -0400

    Updated routes

---
+++ client/frontend/src/views/login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="login">
+    <login />
+  </div>
+</template>
+
+<script>
+import login from '../components/auth/login.vue'
+
+export default {
+  name: 'App',
+  components: {
+    login
+  }
+}
+</script>
