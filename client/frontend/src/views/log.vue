bac802c96288af487f67593d50b8c51c1216851d
Evans Yeboah Jr <eyeboahcw@gmail.com>
2020-10-08 23:34:33 -0400

    added log template

---
+++ client/frontend/src/views/log.vue
@@ -0,0 +1,54 @@
+<template>
+    <v-app style="">
+        <v-container fluid class="white lighten-5" >
+        <v-col
+        :align="align"
+        no-gutters
+      >
+        <v-col class="col"
+          v-for="n in 2"
+          :key="n"
+          style="margin-bottom: 38vh;"
+        >
+          <v-card
+            class="pa-2" 
+            style="box-shadow: 0px 9.5px 15px -7px #888888;height: 55vh; margin-bottom: 15vh"
+            outlined
+            tile
+          >
+            One of three columns
+          </v-card>
+        </v-col>
+      </v-col>
+    </v-container>
+    </v-app>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return{
+            count: 0
+        }
+    },
+
+    methods: {
+        addLog(){
+            let token = localStorage.getItem("jwt");
+            return token;
+        }
+    }
+}
+
+
+
+
+</script>
+
+<style scoped>
+.col{
+    box-shadow: "-6px 9.5px 20px -7px #888888";
+    height: 10em;
+}
+</style>
