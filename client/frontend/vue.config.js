126d993fc873e87671e914c403d8f17e953f404b
Evans Yeboah Jr <eyeboahcw@gmail.com>
2020-11-11 12:53:57 -0500

    updated configs

--- client/frontend/vue.config.js
+++ client/frontend/vue.config.js
@@ -3,6 +3,6 @@ module.exports = {
     "vuetify"
   ],
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/WebProgramming-Exercise-App-/'
+    ? '/WebProgramming-Exercise-App-/dist'
     : '/'
 }
