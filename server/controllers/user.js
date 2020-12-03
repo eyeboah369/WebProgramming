212962c827ab4d83b4467ca489336f9052cc8ba6
Evans Yeboah Jr <eyeboahcw@gmail.com>
2020-12-03 14:17:41 -0500

    Updates and comments for dynamic query

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -3,9 +3,18 @@ require("dotenv").config();
 const User = require("../models/user");
 const config = require('../config/auth');
 
-exports.getUserInfo = async (req, res) => {
-    console.log(req.body.password);
-    await res.json(req.body);
+exports.getList = async (req, res) => {
+    const list = await User.findOne({_id: "5fc92e70c4d7ba8ab66a0632"});
+    console.log(list.exercises);
+    /*list.exercises = ['pushups','curlups','jumping jacks','pullups','planks','leg up',
+    'situps','benchpress','weights','squats','jumprope'] */
+
+    //here is where the regular expression call would be executed to query the database based on user input
+    //This is where the (req) aspect would store a char for every input into the text field
+    //MongoDB allows for users to be able to make simple async request to the database to be able to find
+    //the data that would match the body of the req using the RegExp() function
+    console.log(list.exercises);
+    res.send(list.likes);
 }
 
 exports.getProfile = async (req, res) => {
